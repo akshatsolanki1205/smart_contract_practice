34d5f33451bf8ba280e448d9533062ed227ac202
AKSHAT <akshatsolanki1205@gmail.com>
2022-12-13 16:23:44 +0530

    some changes done

--- solidity.sol
+++ solidity.sol
@@ -3,25 +3,49 @@
 pragma solidity ^0.8.9;
 
 contract bank {
-
     struct account {
         address owner;
         uint balance;
-        uint accountcreatedTime;
+        uint accountcreationTime;
     }
 
-    mapping(address=> account) public LaxmiChitFund;
+    mapping(address => account) public LaxmiChitFund;
 
     event deposit(address owner, uint amount, uint timestamp);
     event withdrawal(address owner, uint amount, uint timestamp);
 
-    //account creation
-
-    function creatAccount() public payable {
-        require(msg.value < 1 ether, "minimum requirement not fulfilled");
-        
+    modifier minimum() {
+        require(msg.value > 1 ether, "minimum requirement not fulfilled");
+        _;
     }
 
+    //account creation
 
+    function creatAccount() public payable minimum {
+        account memory A;
+        A.owner = msg.sender;
+        A.balance += msg.value;
+        A.accountcreationTime = block.timestamp;
+        LaxmiChitFund[msg.sender] = A;
+        emit deposit(msg.sender, msg.value, block.timestamp);
+    }
 
+    // deposit
+
+    function depositing() public payable minimum {
+        LaxmiChitFund[msg.sender].balance += msg.value;
+        emit deposit(msg.sender, msg.value, block.timestamp);
+    }
+
+    // withdraw
+
+    function withdraw(uint amount) public payable {
+        require(
+            amount < LaxmiChitFund[msg.sender].balance,
+            " not sufficient balance"
+        );
+        payable(msg.sender).transfer(amount);
+        LaxmiChitFund[msg.sender].balance -= amount;
+        emit withdrawal(msg.sender, amount, block.timestamp);
+    }
 }
