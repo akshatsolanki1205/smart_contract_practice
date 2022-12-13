cd318c476a90e4f4865e770928403f3a068c54d2
AKSHAT <akshatsolanki1205@gmail.com>
2022-12-13 15:04:47 +0530

    some changes done

--- solidity.sol
+++ solidity.sol
@@ -4,6 +4,24 @@ pragma solidity ^0.8.9;
 
 contract bank {
 
-    
+    struct account {
+        address owner;
+        uint balance;
+        uint accountcreatedTime;
+    }
+
+    mapping(address=> account) public LaxmiChitFund;
+
+    event deposit(address owner, uint amount, uint timestamp);
+    event withdrawal(address owner, uint amount, uint timestamp);
+
+    //account creation
+
+    function creatAccount() public payable {
+        require(msg.value < 1 ether, "minimum requirement not fulfilled");
+        
+    }
+
+
 
 }
